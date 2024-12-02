1c2b7de484491c38ebdda0df6c15299f6d19555d
Tim Tomczek <>
2024-12-02 15:30:30 +0100

    finished create-vms script

--- create-vms.ps1
+++ create-vms.ps1
@@ -1,10 +1,29 @@
 param (
-    [string]$servers,
+    [string]$serversAndPorts,
     [string]$apiToken,
     [string]$node,
     [string]$iso,
     [int]$vmCount = 1
 )
+
+# Parameter abfragen, falls nicht angegeben
+if (-not $serversAndPorts) {
+	$serversAndPorts = Read-Host -Prompt 'server:port,server:port...'
+	if (-not $serversAndPorts) { exit 1 }
+}
+if (-not $apiToken) { 
+	$apiTokenSecure = Read-Host -Prompt 'TokenId=secret' -AsSecureString
+	if (-not $apiTokenSecure) { exit 1 }
+	$apiToken = (ConvertFrom-SecureString $apiTokenSecure -AsPlainText)
+}
+if (-not $node) {
+	$node = Read-Host -Prompt 'Nodename'
+	if (-not $node) { exit 1 }
+}
+if (-not $iso) {
+	$iso = Read-Host -Prompt 'ISO Datei (z.B. local:iso/deine-iso-datei.iso)'
+	if (-not $iso) { exit 1 }
+}
 
 # Installiere das Modul, falls noch nicht geschehen
 if (-not (Get-Module -ListAvailable -Name Corsinvest.ProxmoxVE.Api)) {
@@ -15,20 +34,14 @@ if (-not (Get-Module -ListAvailable -Name Corsinvest.ProxmoxVE.Api)) {
 # Importiere das Modul
 Import-Module Corsinvest.ProxmoxVE.Api
 
-# Parameter abfragen, falls nicht angegeben
-if (-not $servers) { $servers = Read-Host -Prompt 'server:port,server:port...' }
-if (-not $apiToken) { $user = Read-Host -Prompt 'TokenId=secret' }
-if (-not $node) { $node = Read-Host -Prompt 'Nodename' }
-if (-not $iso) { $iso = Read-Host -Prompt 'ISO Datei (z.B. local:iso/deine-iso-datei.iso)' }
-
 # Verbinde dich mit dem Proxmox VE Cluster
-$ticket = Connect-PveCluster -HostsAndPorts $servers -ApiToken $apiToken -SkipCertificateCheck
+$ticket = Connect-PveCluster -HostsAndPorts $serversAndPorts -ApiToken $apiToken -SkipCertificateCheck
 
-$firstUnusedVmId(Get-PveClusterNextid -PveTicket $using:ticket).response.data
+$firstUnusedVmId = (Get-PveClusterNextid -PveTicket $ticket).response.data
 
-1..$vmCount | ForEach-Object -Parallel {
+$result = 1..$vmCount | ForEach-Object -Parallel {
 	
-	$nextVMID = $using:firstUnusedVmId + $($_)
+	[int]$nextVMID = [int]$using:firstUnusedVmId + [int]$($_)-1
 	$vmName = "WebserverVM-$nextVMID"
 	
 	$sata = @{
@@ -36,28 +49,29 @@ $firstUnusedVmId(Get-PveClusterNextid -PveTicket $using:ticket).response.data
 	}
 	
 	$net = @{
-		0 = 'virtio,bridge=vmbr0;firewall=1'
+		0 = 'virtio,bridge=vmbr0,firewall=1'
 	}
 	
-	$createdVm = New-PveNodesQemu -PveTicket $using:ticket -Node $using:node -VMID $nextVMID -Name $vmName -Memory 4096 -Cores 4 -SataN $sata -cdrom $using:iso -NetN $net -Start -Ostype l26
+	$createResult = New-PveNodesQemu -PveTicket $using:ticket -Node $using:node -VMID $nextVMID -Name $vmName -Memory 4096 -Cores 4 -SataN $sata -cdrom $using:iso -NetN $net -Start -Ostype l26 -Agent 1
+	if (-not $createResult.IsSuccessStatusCode) {
+		Write-Host "Erstellen der VM fehlgeschlagen"
+		Write-Host $createResult
+		Exit 1
+	}
 	
     # Warte, bis die VM gestartet ist
-	Write-Output "Warte 240 Sekunden auf VM start"
-    Start-Sleep -Seconds 240
+	Write-Host "Warte 250 Sekunden auf VM start"
+    Start-Sleep -Seconds 250
 
     # Hole die IP-Adresse der VM
-    $vmStatus = Get-PveNodesQemuConfig -Node $using:node -VMID $nextVMID -PveTicket ticket -Current true
-    $vmIP = $vmStatus.Network | Where-Object { $_.name -eq 'net0' } | Select-Object -ExpandProperty 'ip-addresses'
+    $vmStatus = Get-PveNodesQemuAgentNetworkGetInterfaces -Node $using:node -VMID $nextVMID -PveTicket $using:ticket
+    $vmIP = $vmStatus.response.data.result[1] | Select-Object -ExpandProperty 'ip-addresses' | Select-Object -Property ip-address -ExpandProperty 'ip-address' -First 1
+	Write-Host "Virtuelle Maschine mit der ID $nextVMID und dem Namen $vmName wurde erstellt und gestartet. Die IP-Adresse ist: $vmIP"
 
     return [PSCustomObject]@{
-        VMID = $VMID
-        Name = $Name
-        IPAddress = $vmIP[0]
+        VMID = $nextVMID
+        Name = $vmName
+        IPAddress = $vmIP
     }
 }
-
-# Ausgabe der Ergebnisse
-$results | ForEach-Object {
-    Write-Host "Virtuelle Maschine mit der ID $($_.VMID) und dem Namen $($_.Name) wurde erstellt und gestartet."
-    Write-Host "Die IP-Adresse der VM ist: $($_.IPAddress)"
-}
+return $result
